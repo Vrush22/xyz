a68eccf4dde7571c409cb6e05f9beb8c1dee90b5
vrushali <vrushalihirudkar1@gmail.com>
2023-10-18 09:32:01 +0530

    ok

---
+++ ProjectwithMaven/src/main/java/com/vrush/ProjectwithMaven/App.java
@@ -0,0 +1,13 @@
+package com.vrush.ProjectwithMaven;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "project started..." );
+       
+        
+        
+    }
+}
